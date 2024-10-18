afa7b251f190ba0d923c193851890adc7cd74daa
botastark <bota.duisenbay@gmail.com>
2024-10-18 13:05:34 +0200

    conversions classes done

--- conversion.py
+++ conversion.py
@@ -12,7 +12,9 @@ import numpy as np
 
 
 class GPS_point:
-    def __init__(self, lat=None, lon=None, alt=None, rad=False) -> None:
+    def __init__(self, data, rad=False) -> None:
+        lat, lon, alt = data
+        self.rad = rad
         if rad:
             self.lon = math.radians(lon)
             self.lat = math.radians(lat)
@@ -22,19 +24,84 @@ class GPS_point:
         self.alt = alt
 
     def inrad(self):
-        return math.radians(self.lat), math.radians(self.lon), self.alt
+        if self.rad == False:
+            return GPS_point(
+                (math.radians(self.lat), math.radians(self.lon), self.alt), rad=True
+            )
+        else:
+            return GPS_point((self.lat, self.lon, self.alt))
+
+    def __str__(self):
+        if self.rad == True:
+            return f"lat: {self.lat}rad, lon: {self.lon}rad, alt: {self.alt}m)"
+        return f"lat: {self.lat}°, lon: {self.lon}°, alt: {self.alt}m"
 
 
-class converter:
+class ECEF_point:
+    def __init__(self, data) -> None:
+        if data is None:
+            data = (0, 0, 0)
+        self.X, self.Y, self.Z = data
+
+    def __str__(self):
+        return f"ECEF_point(X: {self.X}, Y: {self.Y}, Z: {self.Z})"
+
+    def __repr__(self):
+        return f"ECEF_point({self.X}, {self.Y}, {self.Z})"
+
+    # Method to subtract two ECEF_point objects
+    def __sub__(self, other):
+        if isinstance(other, ECEF_point):
+            # Subtract the corresponding attributes and return a new ECEF_point
+            return ECEF_point((self.X - other.X, self.Y - other.Y, self.Z - other.Z))
+        else:
+            raise TypeError("Subtraction is only supported between ECEF_point objects.")
+
+    def to_array(self):
+        return np.array([self.X, self.Y, self.Z])
+
+
+class WRF_point:
+    def __init__(self, data, frame="NED"):
+        self.x, self.y, self.z = data
+        self.frame = frame
+
+    def __str__(self):
+        return f"WRF_point(x: {self.x}, y: {self.y}, z: {self.z}, frame: {self.frame})"
+
+    def __repr__(self):
+        return f"WRF_point({self.x}, {self.y}, {self.z}, frame={self.frame})"
+
+    # Function to convert from NED to ENU
+    def to_ENU(self):
+        if self.frame == "NED":
+            enu_x = self.y  # East (from Y in NED)
+            enu_y = self.x  # North (from X in NED)
+            enu_z = -self.z  # Up (reverse of Z in NED)
+            return WRF_point((enu_x, enu_y, enu_z), frame="ENU")
+        else:
+            print("Already in ENU frame.")
+            return self
+
+    # Function to convert from ENU to NED
+    def to_NED(self):
+        if self.frame == "ENU":
+            ned_x = self.y  # North (from Y in ENU)
+            ned_y = self.x  # East (from X in ENU)
+            ned_z = -self.z  # Down (reverse of Z in ENU)
+            return WRF_point(ned_x, ned_y, ned_z)
+        else:
+            print("Already in NED frame.")
+            return self
+
+
+class Converter:
     def __init__(self, ref_path):
-        self.ref_geodetic = GPS_point(
-            self.extract_gps_data(self.get_image_properties(ref_path)["GPSInfo"])
-        )
-        self.ref_ECEF = geodetic2ecef(self.ref_geodetic.inrad())
+        self.ref_geodetic = self.extract_gps_data(ref_path)
+        print(self.ref_geodetic)
+        self.ref_ECEF = self.geodetic2ecef(self.ref_geodetic.inrad())
 
-        # print(f"Ref point ECEF Coordinates: {ecef_coords}")
-
-        pass
+        print(self.ref_ECEF)
 
     def get_image_properties(self, image_path):
         img = Image.open(image_path)
@@ -67,7 +134,8 @@ class converter:
         return image_data
 
     # Extract and convert GPS coordinates from the EXIF GPSInfo
-    def extract_gps_data(self, gps_info):
+    def extract_gps_data(self, image_path):
+        gps_info = self.get_image_properties(image_path)["GPSInfo"]
         lat_dms = gps_info["GPSLatitude"]
         lat_ref = gps_info["GPSLatitudeRef"]
         lon_dms = gps_info["GPSLongitude"]
@@ -77,7 +145,7 @@ class converter:
         lat_dd = self._dms_to_dd(lat_dms, lat_ref)  # Latitude in decimal degrees
         lon_dd = self._dms_to_dd(lon_dms, lon_ref)  # Longitude in decimal degrees
 
-        return lat_dd, lon_dd, altitude
+        return GPS_point((lat_dd, lon_dd, altitude))
 
     # Function to convert degrees, minutes, seconds to decimal degrees
     def _dms_to_dd(self, dms_tuple, ref):
@@ -89,84 +157,49 @@ class converter:
             decimal_degrees = -decimal_degrees
         return decimal_degrees
 
+    def radius_n(self, lat):
+        cot = math.cos(lat) / math.sin(lat)
+        return a / (1 - e_sq / (1 + cot * cot)) ** 0.5
 
-def radius_n(lat):
-    cot = math.cos(lat) / math.sin(lat)
-    return a / (1 - e_sq / (1 + cot * cot)) ** 0.5
+    def geodetic2ecef(self, point):
+        N = self.radius_n(point.lat)
+        out = ECEF_point(None)
+        out.X = (N + point.alt) * math.cos(point.lat) * math.cos(point.lon)
+        out.Y = (N + point.alt) * math.cos(point.lat) * math.sin(point.lon)
+        out.Z = ((1 - e_sq) * N + point.alt) * math.sin(point.lat)
+        return out
 
+    def ecef2ned(self, p):
+        sin_lat = math.sin(self.ref_geodetic.lat)
+        cos_lat = math.cos(self.ref_geodetic.lat)
 
-def geodetic2ecef(lat, lon, h):
-    N = radius_n(lat)
-    X = (N + h) * math.cos(lat) * math.cos(lon)
-    Y = (N + h) * math.cos(lat) * math.sin(lon)
-    Z = ((1 - e_sq) * N + h) * math.sin(lat)
-    return (X, Y, Z)
+        sin_lon = math.sin(self.ref_geodetic.lon)
+        cos_lon = math.cos(self.ref_geodetic.lon)
 
-
-def ecef2ned(p, p_ref, lon_ref, lat_ref):
-    sin_lat = math.sin(lat_ref)
-    cos_lat = math.cos(lat_ref)
-
-    sin_lon = math.sin(lon_ref)
-    cos_lon = math.cos(lon_ref)
-
-    R = np.array(
-        [
-            [-sin_lat * cos_lon, -sin_lat * sin_lon, cos_lat],
-            [-sin_lon, cos_lon, 0],
-            [-cos_lat * cos_lon, -cos_lat * sin_lon, -sin_lat],
-        ]
-    )
-    assert p.shape == p_ref.shape
-
-    return np.dot(R, (p - p_ref))
+        R = np.array(
+            [
+                [-sin_lat * cos_lon, -sin_lat * sin_lon, cos_lat],
+                [-sin_lon, cos_lon, 0],
+                [-cos_lat * cos_lon, -cos_lat * sin_lon, -sin_lat],
+            ]
+        )
+        return WRF_point(np.dot(R, (p - self.ref_ECEF).to_array()))
 
 
-# testing
-"""
-Given:
-    Ref point: lon, lat, h(ASML)
-    Point p: lon, lat, h(ASML)
-Get:
-    Point p: x, y, z (NED), wrt Ref point is (0,0,0) NED
-"""
+# DESKTOP = "/home/bota/Desktop/wheat"
 
+rimage_path = (
+    "/home/bota/Desktop/wheat/APPEZZAMENTO PICCOLO/DJI_20240607121127_0003_D.JPG"
+)
 
-rimage_path = "/workspaces/real2map/APPEZZAMENTO PICCOLO/DJI_20240607121127_0003_D.JPG"
+conv = Converter(rimage_path)
+sec_image_path = (
+    "/home/bota/Desktop/wheat/APPEZZAMENTO PICCOLO/DJI_20240607121129_0004_D_point0.JPG"
+)
 
-conv = Converter(rimahe_path)
-# sec_image_path = (
-#     "/workspaces/real2map/APPEZZAMENTO PICCOLO/DJI_20240607121129_0004_D_point0.JPG"
-# )
-
-# rimage_properties = get_image_properties(rimage_path)
-# # print(rimage_properties["GPSInfo"])
-
-# lat_dd, lon_dd, altitude = extract_gps_data(rimage_properties["GPSInfo"])
-# # print(f"Latitude: {lat_dd}°, Longitude: {lon_dd}°, Altitude: {altitude} m")
-
-# # Convert geodetic to ECEF
-# ecef_coords = np.array(
-#     geodetic2ecef(math.radians(lat_dd), math.radians(lon_dd), altitude)
-# )  # .reshape((3, 1))
-# print(f"Ref point ECEF Coordinates: {ecef_coords}")
-
-
-# simage_properties = get_image_properties(sec_image_path)
-# # print(simage_properties["GPSInfo"])
-
-# slat_dd, slon_dd, saltitude = extract_gps_data(simage_properties["GPSInfo"])
-# # print(f"second Latitude: {slat_dd}°, Longitude: {slon_dd}°, Altitude: {saltitude} m")
-
-# # # Convert geodetic to ECEF
-# secef_coords = np.array(
-#     geodetic2ecef(math.radians(slat_dd), math.radians(slon_dd), saltitude)
-# )  # .reshape((3, 1))
-# print(f"2nd point ECEF Coordinates: {secef_coords}")
-
-# # Obtain NED of s point given that ref point is (0,0) of NED
-# r_ned = ecef2ned(ecef_coords, ecef_coords, lon_dd, lat_dd)
-# print(r_ned)
-
-# s_ned = ecef2ned(secef_coords, ecef_coords, lon_dd, lat_dd)
-# print(s_ned)
+p = conv.ecef2ned(conv.geodetic2ecef(conv.extract_gps_data(sec_image_path).inrad()))
+print(p)
+print(p.to_ENU())
+r = conv.ecef2ned(conv.geodetic2ecef(conv.extract_gps_data(rimage_path).inrad()))
+print(r)
+print(r.to_ENU())
